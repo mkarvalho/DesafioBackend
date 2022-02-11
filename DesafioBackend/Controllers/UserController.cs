a0587c654f7e98c9d06587a604d4097a0f21d242
Matheus Carvalho <carvalhomath95@gmail.com>
2022-02-11 08:14:01 -0300

    feat: create UserController

---
+++ DesafioBackend/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using DesafioBackend.Services.Interfaces;
+using DesafioBackend.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DesafioBackend.Controllers
+{
+    [ApiController]
+    [Route("api/v1/users")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll(
+            [FromServices] IUserService userService
+            )
+        {
+            var allProfiles = await userService.GetAll();
+
+            return Ok(new ResultViewModel(allProfiles));
+        }
+    }
+}
