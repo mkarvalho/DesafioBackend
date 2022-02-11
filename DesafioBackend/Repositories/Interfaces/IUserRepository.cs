34a510124ee4df6e0dfe83982dbf2af47875e63c
Matheus Carvalho <carvalhomath95@gmail.com>
2022-02-11 08:04:23 -0300

    feat: create User Repository

---
+++ DesafioBackend/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using DesafioBackend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DesafioBackend.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAll();
+        Task<User> GetById(Guid id);
+        Task<User> Create(User user);
+        Task<User> Update(User user);
+        Task Remove(Guid id);
+    }
+}
---
+++ DesafioBackend/Repositories/UserRepository.cs
@@ -0,0 +1,74 @@
+﻿using Dapper;
+using DesafioBackend.Data;
+using DesafioBackend.Entities;
+using DesafioBackend.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioBackend.Repositories
+{
+    public class UserRepository : DapperDbContext, IUserRepository
+    {
+        private readonly EFDbContext _dbContext;
+        public UserRepository(IConfiguration configuration, EFDbContext dbContext) : base(configuration)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<User>> GetAll()
+        {
+            using IDbConnection conn = DapperConnection;
+            conn.Open();
+            var sql = "SELECT * FROM \"Users\"";
+            var result = await conn.QueryAsync<User>(sql);
+            return result.OrderBy(x => x.Name).ToList();
+        }
+
+        public async Task<User> GetById(Guid id)
+        {
+            using IDbConnection conn = DapperConnection;
+            conn.Open();
+            var sql = $"SELECT * FROM \"Users\" WHERE \"Id\" = @id";
+            var result = await conn.QueryAsync<User>(sql, new { id });
+            return result.FirstOrDefault();
+        }
+
+        public async Task<User> Create(User user)
+        {
+            user.Created = DateTime.Now;
+            user.Modified = user.Created;
+            _dbContext.Add(user);
+            await _dbContext.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task Remove(Guid id)
+        {
+            var user = await GetById(id);
+            if (user != null)
+            {
+                _dbContext.Remove(user);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<User> Update(User user)
+        {
+            var profileBD = await GetById(user.Id);
+
+            user.Created = profileBD.Created;
+            user.Modified = DateTime.Now;
+            user.LastLogin = profileBD.LastLogin;
+
+
+            _dbContext.Entry(user).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            return user;
+        }
+    }
+}
